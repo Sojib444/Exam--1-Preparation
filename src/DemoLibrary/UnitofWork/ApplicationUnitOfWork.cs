5ee93dc2d785022db4d68bdf210b30c8907d9cfb
Sojib <mdsojibhosen444@gmail.com>
2022-09-16 01:46:41 +0600

    commit

---
+++ src/DemoLibrary/UnitofWork/ApplicationUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DemoLibrary.Repository;
+using DemoWebLibrary.ApplicationDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.UnitofWork
+{
+    public class ApplicationUnitOfWork : UnitofWork, IApplicationUnitofWork
+    {
+        public IStudentRepository student { get; private set; }
+        public ApplicationUnitOfWork(StudentDbContext context,IStudentRepository studentRepository)
+            :base(context)
+        {
+            student = studentRepository;
+        }
+        
+
+    }
+}
---
+++ src/DemoLibrary/UnitofWork/UnitofWork.cs
@@ -0,0 +1,29 @@
+﻿using DemoWebLibrary.ApplicationDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.UnitofWork
+{
+    public class UnitofWork : IUnitofWork
+    {
+        private readonly StudentDbContext _context;
+
+        public UnitofWork(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
